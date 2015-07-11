daaed19ef6a91cab39449c212d8d76166f1186dc
Kilovice <xxkilovicexx@gmail.com>
2015-07-10 20:29:17 -0400

    Update OPConfig.java

--- OPConfig.java
+++ OPConfig.java
@@ -1,8 +1,24 @@
 package dev.kilovice.opsecurity.main;
 
+import java.io.File; 
 import java.util.List;
+import java.util.Map;
+
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class OPConfig {
+	
+	public static FileConfiguration getOPConfig()
+	{
+		return OPSecurity.config;
+	}
+	
+	public static File getOPConfigFile()
+	{
+		File f = new File(OPSecurity.getInstance().getDataFolder() + File.separator + "config.yml");
+		return f;
+	}
+	
 	
 	public static boolean enabled = OPSecurity.config.getBoolean("config.enabled");
 	public static boolean checkOP = OPSecurity.config.getBoolean("config.check-op");
@@ -17,6 +33,15 @@ public class OPConfig {
 	public static long timerDelay = OPSecurity.config.getLong("config.timer-delay");
 	public static long timerInterval = OPSecurity.config.getLong("config.timer-interval");
 	public static String kickMsg = OPSecurity.config.getString("config.kick-msg");
+	public static boolean cmdpw = OPSecurity.config.getBoolean("config.command-password");
+	public static boolean player_deop = OPSecurity.config.getBoolean("config.de-op");
+	public static String cmdtp = OPSecurity.config.getString("config.type");
+	public static String globalpw = OPSecurity.config.getString("config.global-password");
+	public static List<String> pwcmds = OPSecurity.config.getStringList("config.protected-commands");
+	public static Map<String, ?> plrpws = OPSecurity.config.getConfigurationSection("config.player-password").getValues(false);
+	public static boolean metrics = OPSecurity.config.getBoolean("config.metrics");
+	public static boolean debug = OPSecurity.config.getBoolean("config.debug");
+	public static boolean updater = OPSecurity.config.getBoolean("config.update-check");
 	public static boolean checkNull(String path){
 		if(OPSecurity.config.getString(path) == null){
 			throw new OPConfigException("Invalid Configuration!", path);
