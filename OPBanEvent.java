6bdb3f79aafd03933f7e5e52245d138869688bf0
Kilovice <xxkilovicexx@gmail.com>
2015-07-10 20:28:50 -0400

    Update OPBanEvent.java

--- OPBanEvent.java
+++ OPBanEvent.java
@@ -1,5 +1,7 @@
 package dev.kilovice.opsecurity.main;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
@@ -10,12 +12,16 @@ public class OPBanEvent extends Event implements Cancellable{
 
 	private Boolean cancelled = false;
 	private static final HandlerList handlers = new HandlerList();
+
+	@SuppressWarnings("deprecation")
 	public OPBanEvent(Player player)
 	{
-		
-		if(cancelled != false){
+		OPDebug.log(this.getClass(), "Event Called.");
+		if(cancelled == true){
+			OPDebug.log(this.getClass(), "Event Cancelled.");
 			return;
 		}
+		OPDebug.log(this.getClass(), "Event not Cancelled.");
 		if(!OPConfig.checkNull("config.commands")){
 			for(String command : OPConfig.cmdList){
 				String cmd = command.replace("%player%", player.getName());
@@ -26,13 +32,14 @@ public class OPBanEvent extends Event implements Cancellable{
 		if(!OPConfig.checkNull("config.perm-msg")){
 			for(String msg : OPConfig.permMsgList){
 				for(Player all : Bukkit.getOnlinePlayers()){
-					all.sendMessage(msg.replace('&', '§').replace("%player%", player.getName()));
+					if(all.hasPermission(OPPermissions.VIEW_BROADCAST))
+					all.sendMessage(ChatColor.translateAlternateColorCodes('&', msg.replace("%player%", player.getName())));
 				}
 		}
 		if(!OPConfig.checkNull("config.bc-msg")){
 			for(String msg : OPConfig.bcMsgList){
 				for(Player all : Bukkit.getOnlinePlayers()){
-					all.sendMessage(msg.replace('&', '§').replace("%player%", player.getName()));
+					all.sendMessage(ChatColor.translateAlternateColorCodes('&', msg.replace("%player%", player.getName())));
 				}
 			}
 		}
@@ -40,7 +47,7 @@ public class OPBanEvent extends Event implements Cancellable{
 			if(OPConfig.player_nameban){
 				player.setBanned(true);
 				if(!OPConfig.checkNull("config.kick-msg")){
-					player.kickPlayer(OPConfig.kickMsg.replace("%player%", player.getName()).replace('&', '§'));
+					player.kickPlayer(ChatColor.translateAlternateColorCodes('&', OPConfig.kickMsg.replace("%player%", player.getName())));
 				}
 			}
 		}
@@ -48,6 +55,11 @@ public class OPBanEvent extends Event implements Cancellable{
 			if(OPConfig.player_ipBan){
 				Bukkit.getServer().banIP(player.getAddress().getAddress().getHostAddress());
 			}
+		}
+		if(!OPConfig.checkNull("config.de-op")){
+		   if(OPConfig.player_deop){
+			   player.setOp(false);
+		   }
 		}
 	}
 	}
