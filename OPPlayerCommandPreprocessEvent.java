8fc88c8fd02f1e10109cba51aad19164f87220eb
Kilovice <xxkilovicexx@gmail.com>
2015-09-29 22:08:16 -0400

    Update OPPlayerCommandPreprocessEvent.java

--- OPPlayerCommandPreprocessEvent.java
+++ OPPlayerCommandPreprocessEvent.java
@@ -1,9 +1,8 @@
 package dev.kilovice.opsecurity.listeners;
- 
-import net.md_5.bungee.api.ChatColor; 
 
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Player; 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
@@ -15,7 +14,13 @@ import dev.kilovice.opsecurity.main.OPSecurity;
 
 public class OPPlayerCommandPreprocessEvent implements Listener{
 
-	@EventHandler
+	private static OPSecurity plugin;
+	public OPPlayerCommandPreprocessEvent(OPSecurity pl)
+	{
+		plugin = pl;
+		OPSecurity.registerEvents(plugin, this);
+	}
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onCmd(PlayerCommandPreprocessEvent e){
 		OPDebug.log(this.getClass(), "Event Fired.");
 		String rawcmd = e.getMessage();
@@ -24,13 +29,13 @@ public class OPPlayerCommandPreprocessEvent implements Listener{
 		Player p = e.getPlayer();
 		if(!OPConfig.checkNull("config.command-password"))
 		{
-			if(!OPConfig.cmdpw)
+			if(!OPConfig.COMMAND_PASSWORD)
 			{
 				return;
 			}
-	    if(OPConfig.pwcmds.contains(cmd[0].substring(1)))
+	    if(OPConfig.PASSWORD_COMMANDS.contains(cmd[0].substring(1)))
 	    {	
-		String s = OPConfig.cmdtp;
+		String s = OPConfig.COMMAND_TYPE;
 		e.setCancelled(true);	
 		if(s.equalsIgnoreCase("player") || s.equalsIgnoreCase("global"))
 			{
@@ -39,12 +44,12 @@ public class OPPlayerCommandPreprocessEvent implements Listener{
 				{
 				case "player":
 					OPDebug.log(this.getClass(), "Password type: 'Player'");
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', OPMessages.rqtppw));
+					p.sendMessage(OPMessages.REQUEST_PLAYER_PASSWORD);
 					break;
 					
 				case "global":
 					OPDebug.log(this.getClass(), "Password type: 'Global'");
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', OPMessages.rqtgpw));
+					p.sendMessage(OPMessages.REQUEST_GLOBAL_PASSWORD);
 					break;
 				}
 				OPDebug.log(this.getClass(), "Saving Command to Map");
