1a578f872ef8bdf04f8563d8e9a44defc129d011
Kilovice <xxkilovicexx@gmail.com>
2016-01-04 15:26:52 -0500

    Update OPPlayerCommandPreprocessEvent.java

--- OPPlayerCommandPreprocessEvent.java
+++ OPPlayerCommandPreprocessEvent.java
@@ -12,7 +12,7 @@ import dev.kilovice.opsecurity.main.OPDebug;
 import dev.kilovice.opsecurity.main.OPMessages;
 import dev.kilovice.opsecurity.main.OPSecurity;
 
-public class OPPlayerCommandPreprocessEvent implements Listener{
+public class OPPlayerCommandPreprocessEvent implements Listener, OPMessages{
 
 	private static OPSecurity plugin;
 	public OPPlayerCommandPreprocessEvent(OPSecurity pl)
@@ -44,12 +44,12 @@ public class OPPlayerCommandPreprocessEvent implements Listener{
 				{
 				case "player":
 					OPDebug.log(this.getClass(), "Password type: 'Player'");
-					p.sendMessage(OPMessages.REQUEST_PLAYER_PASSWORD);
+					p.sendMessage(REQUEST_PLAYER_PASSWORD);
 					break;
 					
 				case "global":
 					OPDebug.log(this.getClass(), "Password type: 'Global'");
-					p.sendMessage(OPMessages.REQUEST_GLOBAL_PASSWORD);
+					p.sendMessage(REQUEST_GLOBAL_PASSWORD);
 					break;
 				}
 				OPDebug.log(this.getClass(), "Saving Command to Map");
