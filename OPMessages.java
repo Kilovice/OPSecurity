4f2257d1b1f37c1b7d5fb353fd55bbbbcfc2dab3
Kilovice <xxkilovicexx@gmail.com>
2016-01-04 15:24:46 -0500

    Update OPMessages.java

--- OPMessages.java
+++ OPMessages.java
@@ -5,17 +5,19 @@ import java.io.File;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class OPMessages {
+public interface OPMessages {
 
 	public static File f = new File(OPSecurity.getInstance().getDataFolder().getAbsolutePath(), File.separator + "messages.yml");
 	public static FileConfiguration MSG_CONFIG = YamlConfiguration.loadConfiguration(f);
 	
-	public static String prefix = MSG_CONFIG.getString("messages.prefix");
-	public static String setpw = prefix + MSG_CONFIG.getString("messages.set-password");
-	public static String rqtppw = prefix + MSG_CONFIG.getString("messages.request-player");
-	public static String rqtgpw = prefix + MSG_CONFIG.getString("messages.request-global");
-	public static String noperm = prefix + MSG_CONFIG.getString("messages.no-permission");
-	public static String inctpw = prefix + MSG_CONFIG.getString("messages.incorrect-password");
-	public static String ctpw = prefix + MSG_CONFIG.getString("messages.correct-password");
+	public static final String PREFIX = OPUtils.parseString(MSG_CONFIG.getString("messages.prefix"));
+	public static final String SET_PASSWORD = PREFIX + OPUtils.parseString(MSG_CONFIG.getString("messages.set-password"));
+	public static final String REQUEST_PLAYER_PASSWORD = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.request-player"));
+	public static final String REQUEST_GLOBAL_PASSWORD = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.request-global"));
+	public static final String NO_PERMISSION = PREFIX + OPUtils.parseString(MSG_CONFIG.getString("messages.no-permission"));
+	public static final String INCORRECT_PASSWORD = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.incorrect-password"));
+	public static final String CORRECT_PASSWORD = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.correct-password"));
+	public static final String KICK_MESSAGE = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.kick"));
+	public static final String BROADCAST_PERMISSION_MESSAGE = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.permission-message"));
+	public static final String BROADCAST_MESSAGE = OPUtils.parseString(PREFIX + MSG_CONFIG.getString("messages.broadcast-message"));
 }
-
